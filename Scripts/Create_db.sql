93326e512e45c43757747d5bd26fc12b169fca38
Darshini26 <65527161+Darshini26@users.noreply.github.com>
2025-04-11 20:11:53 +0530

    added test101 schema

--- Scripts/Create_db.sql
+++ Scripts/Create_db.sql
@@ -3,3 +3,5 @@ CREATE OR ALTER SCHEMA {{environment}}.LOAD;
 create  OR ALTER schema {{environment}}.remote_test;
 
 create  OR ALTER schema {{environment}}.test;
+
+create or alter schema {{environment}}.test101;
