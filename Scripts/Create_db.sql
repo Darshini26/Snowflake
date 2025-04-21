97bfd9f132cfae32982983b2524c4cb686f457df
Darshini <darshini.n@cittabase.com>
2025-04-21 11:08:24 +0530

    added schema

--- Scripts/Create_db.sql
+++ Scripts/Create_db.sql
@@ -5,3 +5,6 @@ create  OR ALTER schema {{environment}}.remote_test;
 create  OR ALTER schema {{environment}}.test;
 
 create  OR ALTER schema {{environment}}.test_214;
+
+create  OR ALTER schema {{environment}}.test_214_test;
+
